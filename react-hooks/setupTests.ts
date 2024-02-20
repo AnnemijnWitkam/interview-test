0bcb2cf19ac1854acc64b8c2c496d03026e23e48
AnnemijnWitkam <annemijn@appjection.nl>
2024-02-20 21:51:49 +0100

    fix test setup

--- react-hooks/setupTests.ts
+++ react-hooks/setupTests.ts
@@ -1,10 +1,10 @@
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-// import matchers from '@testing-library/jest-dom/matchers';
-import '@testing-library/jest-dom';
+import matchers from '@testing-library/jest-dom/matchers';
+import "@testing-library/jest-dom";
 
 // extends Vitest's expect method with methods from react-testing-library
-// expect.extend(matchers);
+ expect.extend(matchers);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
